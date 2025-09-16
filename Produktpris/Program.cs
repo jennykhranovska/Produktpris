2ca50a6bdb589726d559c6428186bd93adc24d28
Jenny Khranovska <jenny.khranovska.syne25lin@edu.tucsweden.se>
2025-09-16 11:19:33 +0200

    Add project files.

---
+++ Produktpris/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace Produktpris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange produktens namn:");
+            string product = Console.ReadLine();
+
+
+            Console.WriteLine("Ange produktens pris:");
+            double price = Convert.ToDouble(Console.ReadLine()); // pris är en variabel som lagrar produktens pris,
+            //convert.toDouble omvandlar det som användaren skriver in till dicimal
+            double tax = 0.25; // tax är en variabel som lagrar momsen
+
+
+            Console.WriteLine("Ange antal produkter");
+            int quantity = Convert.ToInt32(Console.ReadLine()); //quantity är en variabel som lagrar antal produkter,
+
+            //jag använder Convert.ToInt32 för att omvandla det användaren skriver in till heltal//
+
+
+
+            double totalPris = CalculateTotal(product, price, quantity, tax);
+            Console.WriteLine("Du köpte " + quantity + " st " + product + ", totalpris (inkl. moms): " + totalPris + " kr");
+
+
+
+            double totalDefaultTax = CalculateTotal(product, price, quantity);
+            Console.WriteLine("Totalpris med default moms 25%: " + totalDefaultTax + " kr");
+
+
+
+        }
+                                                
+         static double CalculateTotal(string product, double price, int quantity, double tax = 0.25)
+        {
+            double total = (price * quantity) * (1 + tax);
+            return total;
+
+        }
+
+    }
+}
